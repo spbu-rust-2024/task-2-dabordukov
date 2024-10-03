1af361a132a6ef28af521f0da6d961f2dfc4d382
Dmitrii Bordukov <zst@localhost>
2024-10-03 21:10:05 +0300

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,12 @@ fn main() {
     let chars: Vec<char> = input.chars().collect();
     let mut borders: (usize, usize) = (0, 0);
     if chars.len() == 0 {
+        println!("");
         return;
     }
     find_longest_palindrome(&chars, &mut borders);
-    let mut i = borders.0;
-    loop {
+    for i in borders.0..borders.1 + 1 {
         print!("{}", chars[i]);
-        i += 1;
-        if i > borders.1 {
-            break;
-        }
     }
 }
 
@@ -24,21 +20,20 @@ fn find_longest_palindrome(string: &Vec<char>, borders: &mut (usize, usize)) {
     let mut left_saved = 0;
     let mut right_saved = 0;
 
-    let mut i = 1;
-    let mut current_length = 1;
-    while i < string.len() {
-        let mut left = i - 1;
-        let mut right = i + 1;
-        while right < string.len() {
-            if string[left] == string[right] {
-                current_length += 2;
-            } else {
-                left += 1;
-                right -= 1;
+    let mut i = 0;
+    while i < string.len() - 1 {
+        let mut left = i;
+        let mut right = i;
+        loop {
+            if string[left] != string[right] {
+                if left != right {
+                    left += 1;
+                    right -= 1;
+                }
                 break;
             }
 
-            if left == 0 {
+            if left == 0 || right == string.len() - 1 {
                 break;
             }
 
@@ -46,25 +41,24 @@ fn find_longest_palindrome(string: &Vec<char>, borders: &mut (usize, usize)) {
             right += 1;
         }
 
-        if current_length > max_length {
-            max_length = current_length;
+        if right - left + 1 > max_length {
+            max_length = right - left + 1;
             left_saved = left;
             right_saved = right;
         }
 
-        current_length = 0;
-        left = i - 1;
-        right = i;
-        while right < string.len() {
-            if string[left] == string[right] {
-                current_length += 2;
-            } else {
-                left += 1;
-                right -= 1;
+        left = i;
+        right = i + 1;
+        loop {
+            if string[left] != string[right] {
+                if right - left != 1 {
+                    right -= 1;
+                    left += 1;
+                }
                 break;
             }
 
-            if left == 0 {
+            if left == 0 || right == string.len() - 1 {
                 break;
             }
 
@@ -72,8 +66,8 @@ fn find_longest_palindrome(string: &Vec<char>, borders: &mut (usize, usize)) {
             right += 1;
         }
 
-        if current_length > max_length {
-            max_length = current_length;
+        if right - left + 1 > max_length {
+            max_length = right - left + 1;
             left_saved = left;
             right_saved = right;
         }
