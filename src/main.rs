0c6188390cc8a57c58de0f5d4fc0def82e4142e5
Dmitrii Bordukov <zst@localhost>
2024-10-07 08:28:55 +0300

    refactor: reuse code in find_longest_palindrome() function

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,38 @@ use std::io;
 fn main() {
     let mut input = String::new();
     let _ = io::stdin().read_line(&mut input);
-    let chars: Vec<char> = input.chars().collect();
+    let chars: Vec<u8> = input.chars().map(|x| x as u8).collect();
     let mut borders: (usize, usize) = (0, 0);
     if chars.len() == 0 {
         println!("");
         return;
     }
-    find_longest_palindrome(&chars, &mut borders);
+
+    find_longest_palindrome(&chars, &mut borders, true);
+    find_longest_palindrome(&chars, &mut borders, false);
+
     for i in borders.0..borders.1 + 1 {
-        print!("{}", chars[i]);
+        print!("{}", chars[i] as char);
     }
 }
 
-fn find_longest_palindrome(string: &Vec<char>, borders: &mut (usize, usize)) {
-    let mut max_length = 1;
-    let mut left_saved = 0;
-    let mut right_saved = 0;
+fn find_longest_palindrome(string: &Vec<u8>, borders: &mut (usize, usize), even: bool) {
+    let mut max_length = borders.1 - borders.0 + 1;
+    let mut left_saved = borders.0;
+    let mut right_saved = borders.1;
 
     let mut i = 0;
     while i < string.len() - 1 {
         let mut left = i;
         let mut right = i;
-        loop {
-            if string[left] != string[right] {
-                if left != right {
-                    left += 1;
-                    right -= 1;
-                }
-                break;
-            }
-
-            if left == 0 || right == string.len() - 1 {
-                break;
-            }
-
-            left -= 1;
+        if even {
             right += 1;
         }
-
-        if right - left + 1 > max_length {
-            max_length = right - left + 1;
-            left_saved = left;
-            right_saved = right;
-        }
-
-        left = i;
-        right = i + 1;
         loop {
             if string[left] != string[right] {
-                if right - left != 1 {
-                    right -= 1;
+                if right - left > 2 {
                     left += 1;
+                    right -= 1;
                 }
                 break;
             }
